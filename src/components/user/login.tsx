e5fc36c9dcf366a59f687bdf49919bee54c7bd19
unknown <a.chnaif2010@gmail.com>
2025-06-21 14:23:23 +0100

    login responsive

--- src/components/user/login.tsx
+++ src/components/user/login.tsx
@@ -127,7 +127,7 @@ export default function Login(){
                         <label htmlFor="email" className="text-left text-sm text-gray-400 mb-1">Adresse email</label>
                         <Input 
                             placeholder="Entrez votre adresse email" 
-                            className="py-3 sm:py-5 border-blue-950 text-blue-950 mb-1 placeholder:text-sm"  
+                            className="w-[90%] sm:w-full py-3 sm:py-5 border-blue-950 text-blue-950 mb-1 placeholder:text-sm placeholder:text-gray-400" 
                             onChange={(e) => handleData("email", e.target.value)}
                             value={login.email}
                         />
@@ -137,7 +137,7 @@ export default function Login(){
                             <Input 
                                 placeholder="Créez votre mot de passe" 
                                 type={passwordVisible ? "" : "password"} 
-                                className="py-3 sm:py-5 border-blue-950 text-blue-950 mb-1 placeholder:text-sm" 
+                                className="w-[90%] sm:w-full py-3 sm:py-5 border-blue-950 text-blue-950 mb-1 placeholder:text-sm placeholder:text-gray-400"
                                 onChange={(e)=> handleData("password", e.target.value)}
                                 value={login.password}
                             />
@@ -161,7 +161,7 @@ export default function Login(){
                         ) : (
                             <Button 
                                 onClick={sendData}
-                                className="w-full bg-orange-500 text-white mb-4 rounded-full cursor-pointer"
+                                className="w-[90%] sm:w-full bg-orange-500 text-white mb-4 rounded-full cursor-pointer"
                             >
                                 Se connecter
                             </Button>
