829cab6265ed0855e6356aae9d6dbfec101ae26a
unknown <a.chnaif2010@gmail.com>
2025-06-21 18:08:36 +0100

    details responsive

--- src/components/user/plans/suggestions.tsx
+++ src/components/user/plans/suggestions.tsx
@@ -38,32 +38,34 @@ export default function Suggestions(){
             </div>
             <div className="h-auto mt-8">
                 <p className="text-center mb-3">Options de transport local</p>
-                <table className="border-collapse border border-gray-400 w-full lg:h-full table-auto">
-                    <thead>
-                        <tr>
-                            <th className="border border-gray-400 px-2 py-1">Transport</th>
-                            <th className="border border-gray-400 px-2 py-1">Coût</th>
-                            <th className="border border-gray-400 px-2 py-1">Impact</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="border border-gray-400 px-2 py-1">Transports commun</td>
-                            <td className="border border-gray-400 px-2 py-1">2.9 Euros</td>
-                            <td className="border border-gray-400 px-2 py-1">Réduit l'empreinte carbone et soutient les services locaux</td>
-                        </tr>
-                        <tr>
-                            <td className="border border-gray-400 px-2 py-1">Marche</td>
-                            <td className="border border-gray-400 px-2 py-1">0 Euro</td>
-                            <td className="border border-gray-400 px-2 py-1">Transport sans émission et soutient la communauté locale</td>
-                        </tr>
-                        <tr>
-                            <td className="border border-gray-400 px-2 py-1">Vélo</td>
-                            <td className="border border-gray-400 px-2 py-1">10 Euros</td>
-                            <td className="border border-gray-400 px-2 py-1">Transport à faible émission de carbone et soutient les infrastructures locales</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="overflow-x-auto">    
+                    <table className="border-collapse border border-gray-400 min-w-full table-auto">
+                        <thead>
+                            <tr>
+                                <th className="border border-gray-400 px-2 py-1">Transport</th>
+                                <th className="border border-gray-400 px-2 py-1">Coût</th>
+                                <th className="border border-gray-400 px-2 py-1">Impact</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="border border-gray-400 px-2 py-1">Transports commun</td>
+                                <td className="border border-gray-400 px-2 py-1">2.9 Euros</td>
+                                <td className="border border-gray-400 px-2 py-1">Réduit l'empreinte carbone et soutient les services locaux</td>
+                            </tr>
+                            <tr>
+                                <td className="border border-gray-400 px-2 py-1">Marche</td>
+                                <td className="border border-gray-400 px-2 py-1">0 Euro</td>
+                                <td className="border border-gray-400 px-2 py-1">Transport sans émission et soutient la communauté locale</td>
+                            </tr>
+                            <tr>
+                                <td className="border border-gray-400 px-2 py-1">Vélo</td>
+                                <td className="border border-gray-400 px-2 py-1">10 Euros</td>
+                                <td className="border border-gray-400 px-2 py-1">Transport à faible émission de carbone et soutient les infrastructures locales</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
